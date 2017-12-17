a1b1d88c68f092f92ed1c471913f223f6fcfb52c
Vincent Benningfield <vincedev8@gmail.com>
2017-12-16 16:24:45 -0800

    Validators, Models

--- Custom_Autos/vehicle/models.py
+++ Custom_Autos/vehicle/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from .validators import validate_category
 
-# Create your models here.
+
+class VehicleProfile(models.Model):
+    make = models.CharField(max_length=120)
+    model = models.CharField(max_length=120)
+    sub_model = models.CharField(max_length=120)
+    year = models.IntegerField(max_length=4)
+    color = models.CharField(max_length=50)
+    category = models.CharField(max_length=120, validators=[validate_category])
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(null=True, blank=True)
+
+    def __str__(self):
+        return self.make
+
